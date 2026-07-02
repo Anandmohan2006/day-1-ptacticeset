cf9671517a4fa756140a5c4f377886666daec6d4
Anand Mohan Dubey <dubeysatyam95296@gmail.com>
2026-07-02 09:29:53 +0530

    Added new topic

---
+++ CHAPTER 2 PRACTICESET/02_problam.py
@@ -0,0 +1,5 @@
+letter = '''dear |NAME|,
+you are selected for job,
+|DATE|'''
+
+print(letter.replace("|NAME|", "anand mohan dubey").replace("|DATE|", "2/06/2026"))
---
+++ CHAPTER 2 PRACTICESET/03_problam.py
@@ -0,0 +1,17 @@
+name = "goku is the strong saiyan wariror"
+
+print (name.find("saiyan")) #.find is used to find the index in the string
+
+ 
+
+print("  ")
+
+
+print (name.replace("strong" ,"strongest"))  # .replace is create a new string with changes
+
+#string are immutable can't change so . replace just create a new string
+
+
+
+
+
