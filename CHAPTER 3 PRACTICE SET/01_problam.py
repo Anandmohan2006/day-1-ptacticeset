f813625783bfeff2d141d59baf4ece0aed167735
Anand Mohan Dubey <dubeysatyam95296@gmail.com>
2026-07-02 13:53:45 +0530

    Added new topic

---
+++ CHAPTER 3 PRACTICE SET/01_problam.py
@@ -0,0 +1,27 @@
+girlfrinds = []
+
+g1 = input("enter the name:")
+girlfrinds.append(g1)
+
+
+g2 = input("enter the name:")
+girlfrinds.append(g2)
+
+
+g3 = input("enter the name:")
+girlfrinds.append(g3)
+
+
+g4 = input("enter the name:")
+girlfrinds.append(g4)
+
+g5 = input("enter the name:")
+girlfrinds.append(g5)
+
+g6 = input("enter the name:")
+girlfrinds.append(g6)
+
+g7 = input("enter the name:")
+girlfrinds.append(g7)
+
+print(girlfrinds)
---
+++ CHAPTER 3 PRACTICE SET/03_problam.py
@@ -0,0 +1,6 @@
+l = [10,50,100,55,21,]
+
+print(sum(l))  # sum the tuple
+
+
+print(len(l))  # len for find the length of tuple
